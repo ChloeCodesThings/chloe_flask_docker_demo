22e808a0a7fc3ab775870c9ce330ea9a0f44840d
chloecodesthings <misschloecondon@gmail.com>
2017-06-07 17:23:30 -0700

    adding to github

---
+++ web/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_whale():
+    return render_template("whale_hello.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
